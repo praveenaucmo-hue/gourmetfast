4f28bd0d8b8518212e478ced9d6d503972bb665d
praveenaucmo-hue <praveena.ucmo@gmail.com>
2025-11-24 18:29:04 -0600

    dim_customers.sql

--- gourmetfast/models/core/dim_customers.sql
+++ gourmetfast/models/core/dim_customers.sql
@@ -1,4 +1,23 @@
 {{ config(materialized='incremental', unique_key='customer_id') }}
 
-select *
-from {{ ref('stg_customers') }}
+with src as (
+    select
+        customer_id,
+        customer_name,
+        email,
+        signup_date
+    from {{ ref('stg_customers') }}
+    {% if is_incremental() %}
+      where signup_date > (
+          select coalesce(max(signup_date), '1900-01-01')
+          from {{ this }}
+      )
+    {% endif %}
+)
+
+select
+    customer_id,
+    customer_name,
+    email,
+    signup_date
+from src
